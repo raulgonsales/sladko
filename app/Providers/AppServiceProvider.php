1bf780ec1e4934ee4c03cbf1a6bd05234504befe
Bohdan Inhliziian <inhliziian@mzk.cz>
2018-05-14 19:51:14 +0200

    add secure assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
-            echo 'test';
-        }
+        //
     }
 
     /**
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,4 +1,4 @@
 <title>App Name - @yield('title')</title>
 <title>App Name - @yield('title')</title>
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<script src="{{ asset('js/app.js') }}"></script>
+<link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+<script src="{{ secure_asset('js/app.js') }}"></script>
