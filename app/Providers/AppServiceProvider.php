4eedd3a6402fd9f83b97d52cc91110b63118076f
Bohdan Inhliziian <inhliziian@mzk.cz>
2018-07-21 14:54:06 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Category;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
 //            dump($query->sql);
 //            dump($query->bindings);
 //        });
+        Schema::defaultStringLength(191);
     }
 
     /**
